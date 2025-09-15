0426f8e1f83fac2c5dd824d8ee86f0a5794e7ea0
Jay Hadiyal <53030868+JaysukhHadiyal@users.noreply.github.com>
2025-09-15 17:30:05 +0000

    Define Veriable

---
+++ 01_basic/01_variable.js
@@ -0,0 +1,13 @@
+const pinCode = 1234; // integer    not changes able this varialble value
+let userName = "Jaysukh"; // string  changes able this varialble value
+var isLoggedIn = false; // boolean  changes able this varialble value
+
+sur_name = "Hadiyal"; // global variable
+
+userName = 7896;
+isLoggedIn = true;
+
+console.log(pinCode);
+console.log(userName);
+console.log(isLoggedIn);
+console.log(sur_name);
