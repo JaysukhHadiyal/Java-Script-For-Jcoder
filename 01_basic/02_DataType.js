abecc996a8a82f9ef6c60a93f9f4ea2633ba4d57
Jay Hadiyal <53030868+JaysukhHadiyal@users.noreply.github.com>
2025-09-15 17:54:39 +0000

    Data Types

---
+++ 01_basic/02_DataType.js
@@ -0,0 +1,49 @@
+// The Data Type in JavaScript
+
+// string
+let name = "Jaysukh"; // double quotes
+let sur_name = 'Hadiyal'; // single quotes
+let address = `India`; // backtick
+
+console.log(name);
+console.log(sur_name);
+console.log(address);
+
+// number
+let pinCode = 361315; // integer
+let amount = 10500.500; // float
+
+console.log(pinCode);
+console.log(amount);
+
+// boolean
+let employee = true;
+let isworker = false;
+
+console.log(employee);
+console.log(isworker);
+
+// null
+let number = null;
+console.log(number);
+
+// undefined
+let result;
+console.log(result);
+
+// check variable data type
+console.log(typeof employee);
+console.log(typeof amount);             
+// object
+let person = {
+  name: "Jaysukh",
+  age: 26,
+  isMarried: true,
+};
+console.log(person);
+console.log(typeof person);     
+
+// array
+let colors = ['red', 'green', 'blue'];
+console.log(colors);
+console.log(typeof colors);
