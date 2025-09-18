bb73e4e19200d645d2f5da6e85497cd8d94232e7
Jay Hadiyal <53030868+JaysukhHadiyal@users.noreply.github.com>
2025-09-18 17:35:21 +0000

    datatype summary

---
+++ 01_basic/datatype_summary.js
@@ -0,0 +1,49 @@
+// Two Type of Datatype is Defined 
+// 1. Primative Datatype
+// 2. None Premative(Reference) Datatype
+
+// Total Include Primitive Datatype is: String, Number, Boolean, BigInt, Symbol, Null, Undefined
+// >>>> Used Memory Stake
+// Total Referece Datatype: Arry(list), Objects(dict), Function
+
+// String Datatype 
+
+console.log('-------------string data type---------------------')
+
+let stringValue = 'Jaysukh'
+
+console.log(stringValue,typeof stringValue)
+
+console.log('-------------Number data type---------------------')
+
+let numberValue = 36
+
+console.log(numberValue, typeof numberValue)
+
+console.log('-------------Boolean data type---------------------')
+
+let isLogging = true
+
+console.log(isLogging, typeof isLogging)
+
+console.log('-------------BigInt data type---------------------')
+
+let bigValue = 123456n
+
+console.log(bigValue, typeof bigValue)
+
+console.log('-------------Null data type---------------------')
+
+let outsideTemp = null
+
+console.log(outsideTemp, typeof outsideTemp)
+
+console.log('-------------undefined data type---------------------')
+
+let my_name;
+
+console.log(my_name, typeof my_name)
+
+
+console.log('-------------Symbol data type---------------------')
+
