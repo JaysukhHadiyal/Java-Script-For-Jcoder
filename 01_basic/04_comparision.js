e0729f2e97eca72424275f514188ae57f069f6ad
Jay Hadiyal <53030868+JaysukhHadiyal@users.noreply.github.com>
2025-09-18 17:16:06 +0000

    add comparision

--- 01_basic/04_comparision.js
+++ 01_basic/04_comparision.js
@@ -1,4 +1,23 @@
 // Comparision
 
+console.log('---------------basic comparision--------------')
 console.log("2" > 1)
+console.log(1 == 1)
+console.log("1" == 1)
 
+console.log('---------------typeof comparision--------------')
+
+console.log('10' === 10)
+console.log(9 === 9)
+
+console.log('---------------datatype(object) Null--------------')
+
+
+console.log(null > 0)
+console.log(null == 0)
+console.log(null >= 0)
+
+console.log('---------------datatype undefined--------------')
+
+console.log(undefined > 0)
+console.log(undefined == 0)
