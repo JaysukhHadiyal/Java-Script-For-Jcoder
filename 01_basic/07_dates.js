f7517b996547d96d22065f151b9c83106020006a
Jay Hadiyal <53030868+JaysukhHadiyal@users.noreply.github.com>
2026-02-01 17:21:59 +0000

    add date

---
+++ 01_basic/07_dates.js
@@ -0,0 +1,11 @@
+// Dates
+
+let newDate = new Date()
+console.log(newDate);
+
+console.log(newDate.toDateString())
+console.log(newDate.toLocaleDateString())
+console.log(newDate.getMonth())
+
+let lDate = Date.now()
+console.log(lDate)
